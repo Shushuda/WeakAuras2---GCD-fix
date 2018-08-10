Shushuda/WeakAuras2---GCD-fix

commit b73e4949796fc5be13f22799b514b296a9e24e99

range check condition for spell and item cooldown

WeakAuras/Prototypes.lua
@@ -2103,6 +2103,18 @@ WeakAuras.event_prototypes = {
         }
       },
       {
+        name = "spellInRange",
+        display = L["Spell in Range"],
+        hidden = true,
+        test = "true",
+        conditionType = "bool",
+        conditionTest = "state and state.show and (UnitExists('target') and WeakAuras.IsSpellInRange(state.spellname, 'target') == 1) == (%s == 1)",
+        conditionEvents = {
+          "PLAYER_TARGET_CHANGED",
+          "WA_SPELL_RANGECHECK",
+        }
+      },
+      {
         hidden = true,
         test = "(genericShowOn == \"showOnReady\" and (startTime == 0 or gcdCooldown)) " ..
         "or (genericShowOn == \"showOnCooldown\" and startTime > 0 and not gcdCooldown) " ..
@@ -2254,10 +2266,12 @@ WeakAuras.event_prototypes = {
       --trigger.itemName = WeakAuras.CorrectItemName(trigger.itemName) or 0;
       trigger.itemName = trigger.itemName or 0;
       local itemName = type(trigger.itemName) == "number" and trigger.itemName or "[["..trigger.itemName.."]]";
-      local ret = [[
-        local startTime, duration, enabled = WeakAuras.GetItemCooldown(%s);
+      local ret = [=[
+        local itemname = [[%s]];
+        local startTime, duration, enabled = WeakAuras.GetItemCooldown(itemname);
         local genericShowOn = %s
-      ]];
+        state.itemname = itemname;
+      ]=];
       if(trigger.use_remaining and trigger.genericShowOn ~= "showOnReady") then
         local ret2 = [[
           local expirationTime = startTime + duration
@@ -2309,6 +2323,18 @@ WeakAuras.event_prototypes = {
         display = L["On Cooldown"],
         conditionType = "bool",
         conditionTest = "(state and state.show and (state.expirationTime and state.expirationTime > GetTime() or state.enabled == 0)) == (%s == 1)",
+      },
+      {
+        name = "itemInRange",
+        display = L["Item in Range"],
+        hidden = true,
+        test = "true",
+        conditionType = "bool",
+        conditionTest = "state and state.show and (UnitExists('target') and IsItemInRange(state.itemname, 'target')) == (%s == 1)",
+        conditionEvents = {
+          "PLAYER_TARGET_CHANGED",
+          "WA_SPELL_RANGECHECK",
+        }
       },
       {
         hidden = true,

WeakAuras/WeakAuras.lua
@@ -1088,14 +1088,24 @@ local function runDynamicConditionFunctions(funcs)
   end
 end
 
-local function handleRedynamicConditions(self, event)
+local function handleDynamicConditions(self, event)
   if (globalDynamicConditionFuncs[event]) then
     for i, func in ipairs(globalDynamicConditionFuncs[event]) do
       func(globalConditionState);
     end
   end
+  if (dynamicConditions[event]) then
+    runDynamicConditionFunctions(dynamicConditions[event]);
+  end
+end
 
-  runDynamicConditionFunctions(dynamicConditions[event]);
+local lastDynamicConditionsUpdateCheck;
+local function handleDynamicConditionsOnUpdate(self)
+  handleDynamicConditions(self, "FRAME_UPDATE");
+  if (not lastDynamicConditionsUpdateCheck or GetTime() - lastDynamicConditionsUpdateCheck > 0.2) then
+    lastDynamicConditionsUpdateCheck = GetTime();
+    handleDynamicConditions(self, "WA_SPELL_RANGECHECK");
+  end
 end
 
 local registeredGlobalFunctions = {};
@@ -1140,12 +1150,19 @@ function WeakAuras.RegisterForGlobalConditions(id)
 
   if (next(register) and not dynamicConditionsFrame) then
     dynamicConditionsFrame = CreateFrame("FRAME");
-    dynamicConditionsFrame:SetScript("OnEvent", handleRedynamicConditions);
+    dynamicConditionsFrame:SetScript("OnEvent", handleDynamicConditions);
     WeakAuras.frames["Rerun Conditions Frame"] = dynamicConditionsFrame
   end
 
   for event in pairs(register) do
-    dynamicConditionsFrame:RegisterEvent(event);
+    if (event == "FRAME_UPDATE" or event == "WA_SPELL_RANGECHECK") then
+      if (not dynamicConditionsFrame.onUpdate) then
+        dynamicConditionsFrame:SetScript("OnUpdate", handleDynamicConditionsOnUpdate);
+        dynamicConditionsFrame.onUpdate = true;
+      end
+    else
+      dynamicConditionsFrame:RegisterEvent(event);
+    end
   end
 end
 

commit b42d48e371e3a0df3e34b37bc1ab93a12146107d

Fix regression in Item Cooldown tracking caused by b73e4949
Github-Issue: 620

WeakAuras/Prototypes.lua
@@ -2297,7 +2297,7 @@ WeakAuras.event_prototypes = {
       trigger.itemName = trigger.itemName or 0;
       local itemName = type(trigger.itemName) == "number" and trigger.itemName or "[["..trigger.itemName.."]]";
       local ret = [=[
-        local itemname = [[%s]];
+        local itemname = %s;
         local startTime, duration, enabled = WeakAuras.GetItemCooldown(itemname);
         local genericShowOn = %s
         state.itemname = itemname;